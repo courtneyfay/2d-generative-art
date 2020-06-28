d48c9e2a4535f007cf336c1ff4b9a7bedb86136e
Courtney Fay <fay.courtney@gmail.com>
2020-06-28 14:27:26 -0600

    adding fun colors generation to the randomized grid

---
+++ colors.js
@@ -0,0 +1,52 @@
+const canvasSketch = require("canvas-sketch");
+// lerp is for linear interpretation
+const { lerp } = require("canvas-sketch-util/math");
+// random is for pseudo-random number generation, like Math.random but super-powered
+const random = require("canvas-sketch-util/random");
+// palettes gives you nice combinations of colors to use together
+const palettes = require("nice-color-palettes");
+const settings = {
+  dimensions: [2048, 2048],
+};
+
+const sketch = () => {
+  const colorCount = random.rangeFloor(1, 6);
+  const palette = random.shuffle(random.pick(palettes)).slice(0, colorCount);
+  const createGrid = () => {
+    const points = [];
+    const count = 40;
+    for (let x = 0; x < count; x++) {
+      for (let y = 0; y < count; y++) {
+        const u = count <= 1 ? 0.5 : x / (count - 1);
+        const v = count <= 1 ? 0.5 : y / (count - 1);
+        points.push({
+          position: [u, v],
+          radius: Math.abs(random.gaussian() * 0.01),
+          color: random.pick(palette),
+        });
+      }
+    }
+    return points;
+  };
+  random.setSeed(512);
+  const points = createGrid().filter(() => random.value() > 0.5);
+  const margin = 400;
+
+  return ({ context, width, height }) => {
+    context.fillStyle = "white";
+    context.fillRect(0, 0, width, height);
+
+    points.forEach(({ position, radius, color }) => {
+      const [u, v] = position;
+      const x = lerp(margin, width - margin, u);
+      const y = lerp(margin, height - margin, v);
+
+      context.beginPath();
+      context.arc(x, y, radius * width, 0, Math.PI * 2, false);
+      context.fillStyle = color;
+      context.fill();
+    });
+  };
+};
+
+canvasSketch(sketch, settings);
